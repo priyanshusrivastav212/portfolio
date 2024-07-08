1af0fe0d1828c6e1f8a79a05c1877ba25f5581ab
Priyanshu Srivastav <136737027+priyanshusrivastav212@users.noreply.github.com>
2024-07-08 13:43:17 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { Metadata } from "next";
+import "./globals.css";
+import "@/assets/css/font.css";
+
+export const metadata: Metadata = {
+  title: "Priyanshu Srivastav | ML Enthusiast | Data Scientist ",
+  description:
+    "Final year CSE undergrad and passionate ML enthusiast, aspiring to be a skilled and innovative data scientist.",
+  icons: {
+    icon: "/favicon.svg",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://nothing.com",
+    title: "Priyanshu Srivastav | ML Enthusiast | Data Scientist ",
+    description:
+      "Final year CSE undergrad and passionate ML enthusiast, aspiring to be a skilled and innovative data scientist.",
+    siteName: "My Website",
+    images: [
+      {
+        url: "/og-image.png",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`font-poppins bg-[#f6f5f4] h-screen overflow-hidden max-md:overflow-y-scroll`}
+      >
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
