3e4722f7ee71f3314afa23c4a2151cc7e429c3d0
Priyanshu Srivastav <136737027+priyanshusrivastav212@users.noreply.github.com>
2024-07-08 14:15:51 +0530

    Update whoami.tsx

--- app/components/whoami.tsx
+++ app/components/whoami.tsx
@@ -6,7 +6,7 @@ export default function Whoami() {
     <section className="overflow-hidden md:h-screen max-lg:h-full max-md:px-5 max-xl:px-8 max-md:pt-28">
       <div className="container flex flex-col justify-center h-full mx-auto max-md:pb-16">
         <div className="pt-3 max-sm:pt-0 text-8xl font-bold max-md:text-center text-[#181a1b] max-md:text-6xl max-md:mb-2">
-          Hello, <br /> I am <br /> Mohamed Ali.
+          Hello, <br /> I am <br /> Priyanshu Srivastav.
         </div>
         <div className="font-inter pb-20 max-md:pb-1 pl-1 flex items-center text-lg max-lg:text-base max-md:text-center text-[#212121]">
           Final year CSE undergrad and passionate ML enthusiast, aspiring to be a <br /> skilled and innovative data scientist.
