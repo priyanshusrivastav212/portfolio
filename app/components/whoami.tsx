3967c5da01ecff3c5dac42b0e7c6a7cd2f7eb685
Priyanshu Srivastav <136737027+priyanshusrivastav212@users.noreply.github.com>
2024-07-08 14:11:13 +0530

    Update whoami.tsx

--- app/components/whoami.tsx
+++ app/components/whoami.tsx
@@ -9,12 +9,7 @@ export default function Whoami() {
           Hello, <br /> I am <br /> Mohamed Ali.
         </div>
         <div className="font-inter pb-20 max-md:pb-1 pl-1 flex items-center text-lg max-lg:text-base max-md:text-center text-[#212121]">
-          Highly motivated and goal-oriented Final-Year engineering student <br />specializing in AI & ML. 
-          Strong programming skills in Python and implementing libraries such <br />
-          as TensorFlow, Keras, OpenCV, Pytorch, etc. As I progress in my academic journey, <br />
-          I am actively seeking internship opportunities to further develop my skills and <br />
-          gain practical experience in the industry. If you are searching for a motivated and <br />
-          adaptable engineering student with a passion for sustainable innovation, I would be delighted to connect with you.
+          Final year CSE undergrad and passionate ML enthusiast, aspiring to be a <br /> skilled and innovative data scientist.
         </div>
         <div className="relative max-md:hidden">
           {/* https://codepen.io/mikewagz/pen/PGXqOg */}
