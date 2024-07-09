b991f3e8976e5ad7c3b5c133a8f8a179df612255
Priyanshu Srivastav <136737027+priyanshusrivastav212@users.noreply.github.com>
2024-07-09 18:25:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ import "@/assets/css/main.css";
 const sections: Section[] = [
   { name: "whoami", domElement: null, component: Whoami },
   { name: "projects", domElement: null, component: Projects },
-  { name: "contact", domElement: null, component: Contact },
   { name: "experience", domElement: null, component: Experience },
+  { name: "contact", domElement: null, component: Contact },
 ];
 
 export default function Home() {
