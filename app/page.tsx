346e3e9b5f2c3a89a22391ad7308bc273a3129cd
Priyanshu Srivastav <136737027+priyanshusrivastav212@users.noreply.github.com>
2024-07-09 18:21:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const sections: Section[] = [
   { name: "projects", domElement: null, component: Projects },
   { name: "contact", domElement: null, component: Contact },
   { name: "experience", domElement: null, component: Experience },
-  { name: "techStack", domElement: null, component: techstack },
+  { name: "techStack", domElement: null, component: Techstack },
 ];
 
 export default function Home() {
