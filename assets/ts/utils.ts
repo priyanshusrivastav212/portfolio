64b94cc30e0de4d5196ab9e48a3ab8c3ee6f2318
Priyanshu Srivastav <136737027+priyanshusrivastav212@users.noreply.github.com>
2025-05-29 22:03:53 +0530

    Update utils.ts

--- assets/ts/utils.ts
+++ assets/ts/utils.ts
@@ -5,24 +5,14 @@ export const openLink = (event: React.MouseEvent, link: string, target: string):
   window.open(link, target);
 }
 
-/*export const scrollToElement = (element: HTMLElement | null, time: number = 9000): void => {
+export const scrollToElement = (element: HTMLElement | null, time: number = 9000): void => {
   if (element) {
     animateScrollTo(element, {
+      maxDuration: time,  // guarantees scroll finishes within this time (in ms)
       cancelOnUserAction: false,
       elementToScroll: window,
-      speed: time,
       easing: t => t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
-    });
-  }
-};*/
-export const scrollToElement = (element: HTMLElement | null, duration: number = 500): void => {
-  if (element) {
-    animateScrollTo(element, {
-      cancelOnUserAction: false,
-      elementToScroll: window,
-      maxDuration: duration, // Set total duration of scroll (ms)
-      easing: t => t // Linear easing or replace with a smoother one
-    });
+  });
   }
 };
 
